ed081fc793d259403bc9a2de2173db7ea404751a
MrLawry <rlawry@southlewis.org>
2022-11-17 14:05:23 -0500

    working

--- quiz.js
+++ quiz.js
@@ -1,52 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var buttons = document.querySelectorAll(".btn").length;
-
-    for (var i = 0; i < buttons ; i++) {
-        document.querySelectorAll(".btn")[i].addEventListener("click", function() {
-            checkAnswer(this);
-        });
-    }
-});
+var buttons = [];
+var choices = [];
+var clicks = 0;
 
 var game = {
     round1:{
         q1:{
             q: "Earth _________ the Sun",
-            o1: "Revolves around",
-            o2: "Rotates around",
+            options: {
+                1: "Revolves around",
+                2: "Rotates around"
+            },
             correct: 1
         },
         q2:{
             q: "The Moon _________ Earth",
-            o1: "Revolves around",
-            o2: "Rotates around",
+            options: {
+                1: "Revolves around",
+                2: "Rotates around"
+            },
             correct: 1
         },
         q3:{
             q: "Earth _________ on its axis",
-            o1: "Revolves around",
-            o2: "Rotates around",
+            options: {
+                1: "Revolves",
+                2: "Rotates"
+            },
             correct: 2
         },
         q4:{
-            q: "Earth _________ on its axis",
-            o1: "Revolves",
-            o2: "Rotates",
+            q: "The Sun appears to move through the sky during the day.",
+            options: {
+                1: "Earth Revolves around the Sun",
+                2: "Earth Rotates on its axis"
+            },
             correct: 2
         },
         q5:{
-            q: "Earth _________ on its axis",
-            o1: "Revolves around",
-            o2: "Rotates around",
-            correct: 2
+            q: "The constellations are changing each month",
+            options: {
+                1: "Earth Revolves around the Sun",
+                2: "Earth Rotates on its axis"
+            },
+            correct: 1
         }
     },
     round2:{
         q1:{
-            q: "Pigs",
-            o1: "Revolves around",
-            o2: "Rotates around",
+            q: "Earth revolves",
+            options: {
+                1: "Around the Sun",
+                2: "On its Axis"
+            },
             correct: 1
+        },
+        q2:{
+            q: "The Moon rotates",
+            options: {
+                1: "Around Earth",
+                2: "On its Axis"
+            },
+            correct: 2
+        },
+        q3:{
+            q: "The stars circle Polaris because Earth...",
+            options: {
+                1: "Revolves around the Sun",
+                2: "Rotates on its Axis"
+            },
+            correct: 2
         }
     }
 }
@@ -57,58 +79,129 @@ var question = 1;
 var questionRef = "q1";
 var roundCorrect = [];
 var handicap = 0;
-
+var correct;
+var ended = false;
 var winCount = 0;
 var newCreated = false;
 
 var questionList = [];
 
 function newQuestion(){
+    generateQuestion();
+    choices = [];
+    console.log(`Round ref:${roundRef} questionRef:${questionRef}`);
+    let question = game[roundRef][questionRef];
     document.getElementById("question").innerHTML = game[roundRef][questionRef].q;
-    document.getElementById("option-1").innerHTML = game[roundRef][questionRef].o1;
-    document.getElementById("option-2").innerHTML = game[roundRef][questionRef].o2;
+    for(let element of Object.keys(question.options)){
+        choices.push(question.options[element]);
+    }
     newCreated = true;
+    correct = game[roundRef][questionRef].correct;
+    clearButtons();
+    buildOptions();
 }
 
-function isRoundComplete(q,r){
-    if(q==r.length){return true;}
+function clearButtons(){
+    var things = document.querySelectorAll('.btn');
+    things.forEach(el => el.remove());
+}
+
+function buildOptions(){
+    choices.forEach(function(value, i){
+        document.getElementById('buttons').innerHTML += `<button class="btn" id="option-${i}" value=${i+1}>${value}</button>`;
+    });
+    document.querySelectorAll(".btn").forEach(el => el.addEventListener("click", function() {
+        checkAnswer(this);
+    }));
+}
+
+function isRoundComplete(q){
+    let t = 0;
+    q.forEach(function(item){
+        if(item == 1){t+=item;}
+    });
+    console.log(`${t} t ${q.length} qlength`);
+    if(t==q.length){return true;}
     return false;
 }
 
-function newRound(r){
-    questionList = [];
-    for(let i = 0; i<Object.keys(r[roundRef]).length; i++){
-        questionList.push(0);
-    }
-}
-
 function checkAnswer(e){
-    console.log(e.value + " value and " + game.round1.q1.correct);
-    if(newCreated == true){
-        if(e.value==game[roundRef][questionRef].correct){
+    if(!ended){
+        clicks++;
+        if(e.value==correct){
             document.getElementById("message").innerHTML = "Correct";
             document.getElementById("message").classList.remove("lose-class");
-            questionList[question] = 1;
-            
+            questionList[question-1] = 1;
+
             question++;
             questionRef = "q"+question;
 
-            if(isRoundComplete(question,Object.keys(game[roundRef]).length)){
+            if(isRoundComplete(questionList)){
+                console.log("NewRound!");
                 round++;
-                roundRef = "round" + round;
+                setRound(round);
             }
-            console.log("yes");
             winCount++;
+            if(!ended){
+                newQuestion();
+            }
         }
         else{
             document.getElementById("message").innerHTML = "Nah.";
             document.getElementById("message").classList.add("lose-class");
             newQuestion();
         }
-        document.getElementById("points").innerHTML = winCount;
+        console.log(questionList + " check answer");
+        updatePoints();
     }
-    else if(newCreated == false){
-        document.getElementById("message").innerHTML = "You already guessed. Make a new Month.";
+    console.log(`Ended=${ended}`);
+}
+
+function startGame(){
+    setRound(1);
+    newQuestion();
+}
+
+function updatePoints(){
+    document.getElementById("points").innerHTML = winCount;
+}
+
+
+
+function setRound(r){
+    if(gameOver()){
+        endGame();
+    }
+    else{
+        round = r;
+        roundRef = `round${round}`;
+        question = 1;
+        questionRef = `q${question}`;
+        questionList = [];
+        for(let i = 0; i<Object.keys(game[roundRef]).length; i++){
+            questionList.push(0);
+        }
+        document.getElementById("round").innerHTML = `Round ${round}`;
     }
 }
 
+function gameOver(){
+    let gameLength = Object.keys(game).length+1;
+    console.log(`Game length=${gameLength}`);
+    if(round == gameLength){return true};
+    return false
+}
+
+function endGame(){
+    ended = true;
+    document.getElementById("message").innerHTML = "Game Over.  You did it.";
+}
+
+function generateQuestion(){
+    let options = [];
+    questionList.forEach(function(element,index){
+        if(element == 0){options.push(index)};
+    });
+    questionRef = `q${options[Math.floor(Math.random()*options.length)]+1}`;
+    console.log(`questionRef=${questionRef}`);
+}
